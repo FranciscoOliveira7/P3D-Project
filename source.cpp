b723017c2b39d6dcf1d351b7cf8270d9a6ae58a1
Francisco Oliveira <64752398+Sincopse@users.noreply.github.com>
2024-05-12 15:21:26 +0100

    Add project files.

---
+++ source.cpp
@@ -0,0 +1,48 @@
+#pragma comment(lib, "glew32s.lib")
+#pragma comment(lib, "glfw3.lib")
+#pragma comment(lib, "opengl32.lib")
+
+#include <iostream>
+using namespace std;
+
+#define GLEW_STATIC
+#include <GL\glew.h>
+
+#define GLFW_USE_DWM_SWAP_INTERVAL
+#include <GLFW\glfw3.h>
+
+int main(void)
+{
+    GLFWwindow* window;
+
+    /* Initialize the library */
+    if (!glfwInit())
+        return -1;
+
+    /* Create a windowed mode window and its OpenGL context */
+    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
+    if (!window)
+    {
+        glfwTerminate();
+        return -1;
+    }
+
+    /* Make the window's context current */
+    glfwMakeContextCurrent(window);
+
+    /* Loop until the user closes the window */
+    while (!glfwWindowShouldClose(window))
+    {
+        /* Render here */
+        glClear(GL_COLOR_BUFFER_BIT);
+
+        /* Swap front and back buffers */
+        glfwSwapBuffers(window);
+
+        /* Poll for and process events */
+        glfwPollEvents();
+    }
+
+    glfwTerminate();
+    return 0;
+}
