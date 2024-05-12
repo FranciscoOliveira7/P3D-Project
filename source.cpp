6f7207776dd0c9cd7326196e9791e09fe6d969c9
Francisco Oliveira <64752398+Sincopse@users.noreply.github.com>
2024-05-12 20:24:05 +0100

    added initial code and libraries to render

--- source.cpp
+++ source.cpp
@@ -1,48 +1,186 @@
+#pragma region includes
+
 #pragma comment(lib, "glew32s.lib")
 #pragma comment(lib, "glfw3.lib")
 #pragma comment(lib, "opengl32.lib")
 
 #include <iostream>
-using namespace std;
+#include <vector>
 
 #define GLEW_STATIC
 #include <GL\glew.h>
 
-#define GLFW_USE_DWM_SWAP_INTERVAL
+#include <gl\GL.h>
+
 #include <GLFW\glfw3.h>
 
-int main(void)
-{
+#include <glm/glm.hpp> // vec3, vec4, ivec4, mat4, ...
+#include <glm/gtc/matrix_transform.hpp> // translate, rotate, scale, perspective, ...
+#include <glm/gtc/type_ptr.hpp> // value_ptr
+
+#include "shader_util.h"
+
+#pragma endregion
+
+#define WIDTH 640
+#define HEIGHT 480
+
+void print_error(int error, const char* description);
+void init(void);
+void display(void);
+
+#define NumBuffers 3 // Vértices, Cores, EBO
+
+GLuint program;
+
+GLuint VAO;
+GLuint Buffers[NumBuffers];
+const GLuint NumVertices = 6 * 4; // 6 faces * 4 vértices
+const GLuint NumIndices = 6 * 2 * 3; // 6 faces * 2 triângulos/face * 3 vértices/triângulo
+
+float rotation = 0;
+
+int main(void) {
     GLFWwindow* window;
 
-    /* Initialize the library */
-    if (!glfwInit())
-        return -1;
+    glfwSetErrorCallback(print_error);
 
-    /* Create a windowed mode window and its OpenGL context */
-    window = glfwCreateWindow(640, 480, "Hello World", NULL, NULL);
-    if (!window)
-    {
+    if (!glfwInit()) return -1;
+
+    window = glfwCreateWindow(WIDTH, HEIGHT, "Bilhar", NULL, NULL);
+    if (!window) {
         glfwTerminate();
         return -1;
     }
 
-    /* Make the window's context current */
     glfwMakeContextCurrent(window);
 
-    /* Loop until the user closes the window */
-    while (!glfwWindowShouldClose(window))
-    {
-        /* Render here */
+    // Inicia o gestor de extensões GLEW
+    glewExperimental = GL_TRUE;
+    glewInit();
+
+    init();
+
+    glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
+
+    while (!glfwWindowShouldClose(window)) {
+
+        glm::mat4 model = glm::mat4(1.0f);
+        model = glm::rotate(model, rotation += 0.005f, glm::vec3(1, 1, 0));
+        glm::mat4 view = lookAt(
+            glm::vec3(0, 0, 10),
+            glm::vec3(0, 0, -1),
+            glm::vec3(0, 1, 0)
+        );
+
+        glm::mat4 mvp = projection * view * model;
+
+        GLint mpvId = glGetProgramResourceLocation(program, GL_UNIFORM, "MVP");
+        glProgramUniformMatrix4fv(program, mpvId, 1, GL_FALSE, glm::value_ptr(mvp));
+
         glClear(GL_COLOR_BUFFER_BIT);
 
-        /* Swap front and back buffers */
-        glfwSwapBuffers(window);
+        display();
 
-        /* Poll for and process events */
+        glfwSwapBuffers(window);
         glfwPollEvents();
     }
 
     glfwTerminate();
     return 0;
+}
+
+void init(void) {
+
+    // Mesa de Bilhar (fiz isto manualmente (not fun)) - Zes
+    GLfloat vertices[NumVertices][3] = {
+        {-1.0f,  1.0f,  1.0f}, { 1.0f,  1.0f,  1.0f},
+        {-1.0f, -1.0f,  1.0f}, { 1.0f, -1.0f,  1.0f},
+
+        {-1.0f,  1.0f, -1.0f}, { 1.0f,  1.0f, -1.0f},
+        {-1.0f, -1.0f, -1.0f}, { 1.0f, -1.0f, -1.0f},
+    };
+
+    GLfloat cores[NumVertices][3] = {
+        { 0.0f,  1.0f,  0.0f }, { 0.0f,  1.0f,  0.0f },
+        { 0.0f,  1.0f,  0.0f }, { 0.0f,  1.0f,  0.0f },
+
+        { 0.0f,  1.0f,  0.0f }, { 0.0f,  1.0f,  0.0f },
+        { 0.0f,  1.0f,  0.0f }, { 0.0f,  1.0f,  0.0f },
+    };
+
+    GLuint indices[NumIndices] = {
+        // Frente
+        0, 1, 2, 1, 2, 3,
+        // Direita
+        1, 3, 7, 1, 5, 7,
+        // Baixo
+        2, 3, 6, 3, 6, 7,
+        // Esquerda
+        0, 2, 4, 2, 4, 6,
+        // Trás
+        4, 5, 6, 5, 6, 7,
+        // Cima
+        0, 1, 4, 1, 4, 5
+    };
+
+    // VAO
+    glGenVertexArrays(1, &VAO);
+    glBindVertexArray(VAO);
+
+    // VBOs
+    glGenBuffers(NumBuffers, Buffers);
+
+    glBindBuffer(GL_ARRAY_BUFFER, Buffers[0]);
+    glBufferStorage(GL_ARRAY_BUFFER, sizeof(vertices) /*2 * 6 * sizeof(float)*/, vertices, 0);
+
+    glBindBuffer(GL_ARRAY_BUFFER, Buffers[1]);
+    glBufferStorage(GL_ARRAY_BUFFER, sizeof(cores) /*3 * 6 * sizeof(float)*/, cores, 0);
+
+    glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, Buffers[2]);
+    glBufferStorage(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, 0);
+
+
+    ShaderInfo shaders[] = {
+        { GL_VERTEX_SHADER,   "triangles.vert" },
+        { GL_FRAGMENT_SHADER, "triangles.frag" },
+        { GL_NONE, NULL }
+    };
+
+    program = LoadShaders(shaders);
+    if (!program) exit(EXIT_FAILURE);
+    glUseProgram(program);
+
+
+    // Ligar os atributos aos shaders
+
+    // Obtém a localização do atributo 'vPosition' no 'programa'.
+    GLint coordsId = glGetProgramResourceLocation(program, GL_PROGRAM_INPUT, "vPosition");
+    GLint coresId = glGetProgramResourceLocation(program, GL_PROGRAM_INPUT, "vColors");
+
+    glBindBuffer(GL_ARRAY_BUFFER, Buffers[0]);
+    glVertexAttribPointer(coordsId, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    glBindBuffer(GL_ARRAY_BUFFER, Buffers[1]);
+    glVertexAttribPointer(coresId, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
+
+    glEnableVertexAttribArray(coordsId);
+    glEnableVertexAttribArray(coresId);
+}
+
+void display(void) {
+    static const float black[] = {
+        0.0f, 0.0f, 0.0f, 0.0f
+    };
+
+    glClearBufferfv(GL_COLOR, 0, black);
+
+    // Vincula (torna ativo) o VAO
+    glBindVertexArray(VAO);
+
+    glDrawElements(GL_TRIANGLES, NumIndices, GL_UNSIGNED_INT, (void*)0);
+}
+
+void print_error(int error, const char* description) {
+    std::cout << description << std::endl;
 }
