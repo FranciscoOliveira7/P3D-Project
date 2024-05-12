20f0727b9f4401ca1ab317a180eb63bc1b0ab902
Francisco Oliveira <64752398+Sincopse@users.noreply.github.com>
2024-05-12 22:09:39 +0100

    Fixed window showing only a single triangle

--- source.cpp
+++ source.cpp
@@ -35,10 +35,20 @@ GLuint program;
 
 GLuint VAO;
 GLuint Buffers[NumBuffers];
-const GLuint NumVertices = 6 * 4; // 6 faces * 4 vértices
+const GLuint NumVertices = 8; // 6 faces * 4 vértices
 const GLuint NumIndices = 6 * 2 * 3; // 6 faces * 2 triângulos/face * 3 vértices/triângulo
 
-float rotation = 0;
+GLfloat rotation = 0.0f;
+
+GLfloat zoom = 20.0f;
+
+void scrollCallBack(GLFWwindow* window, double xoffset, double yoffset) {
+
+    if (yoffset == 1) zoom -= fabs(zoom) * 0.1f;
+    else if (yoffset == -1) zoom += fabs(zoom) * 0.1f;
+
+    std::cout << "Zoom = " << zoom << std::endl;
+}
 
 int main(void) {
     GLFWwindow* window;
@@ -59,6 +69,8 @@ int main(void) {
     glewExperimental = GL_TRUE;
     glewInit();
 
+    glfwSetScrollCallback(window, scrollCallBack);
+
     init();
 
     glm::mat4 projection = glm::perspective(glm::radians(45.0f), (float)WIDTH / (float)HEIGHT, 0.1f, 100.0f);
@@ -66,9 +78,10 @@ int main(void) {
     while (!glfwWindowShouldClose(window)) {
 
         glm::mat4 model = glm::mat4(1.0f);
-        model = glm::rotate(model, rotation += 0.005f, glm::vec3(1, 1, 0));
+        model = glm::rotate(model, rotation += 0.005f, glm::vec3(0, 1, 0));
+        model = glm::translate(model, glm::vec3(0, -2, 0));
         glm::mat4 view = lookAt(
-            glm::vec3(0, 0, 10),
+            glm::vec3(0, 0, zoom),
             glm::vec3(0, 0, -1),
             glm::vec3(0, 1, 0)
         );
@@ -78,7 +91,7 @@ int main(void) {
         GLint mpvId = glGetProgramResourceLocation(program, GL_UNIFORM, "MVP");
         glProgramUniformMatrix4fv(program, mpvId, 1, GL_FALSE, glm::value_ptr(mvp));
 
-        glClear(GL_COLOR_BUFFER_BIT);
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT);
 
         display();
 
@@ -91,27 +104,29 @@ int main(void) {
 }
 
 void init(void) {
+    glEnable(GL_DEPTH_TEST);
 
     // Mesa de Bilhar (fiz isto manualmente (not fun)) - Zes
     GLfloat vertices[NumVertices][3] = {
-        {-1.0f,  1.0f,  1.0f}, { 1.0f,  1.0f,  1.0f},
-        {-1.0f, -1.0f,  1.0f}, { 1.0f, -1.0f,  1.0f},
+        {-9.0f,  0.5f,  5.5f }, { 9.0f,  0.5f,  5.5f },
+        {-9.0f, -0.5f,  5.5f }, { 9.0f, -0.5f,  5.5f },
 
-        {-1.0f,  1.0f, -1.0f}, { 1.0f,  1.0f, -1.0f},
-        {-1.0f, -1.0f, -1.0f}, { 1.0f, -1.0f, -1.0f},
+        {-9.0f,  0.5f, -5.5f }, { 9.0f,  0.5f, -5.5f },
+        {-9.0f, -0.5f, -5.5f }, { 9.0f, -0.5f, -5.5f },
     };
 
     GLfloat cores[NumVertices][3] = {
-        { 0.0f,  1.0f,  0.0f }, { 0.0f,  1.0f,  0.0f },
-        { 0.0f,  1.0f,  0.0f }, { 0.0f,  1.0f,  0.0f },
+        { 0.4f, 0.8f, 0.5f }, { 0.4f, 0.8f, 0.5f },
+        { 0.4f, 0.8f, 0.5f }, { 0.4f, 0.8f, 0.5f },
 
-        { 0.0f,  1.0f,  0.0f }, { 0.0f,  1.0f,  0.0f },
-        { 0.0f,  1.0f,  0.0f }, { 0.0f,  1.0f,  0.0f },
+        { 0.4f, 0.8f, 0.5f }, { 0.4f, 0.8f, 0.5f },
+        { 0.4f, 0.8f, 0.5f }, { 0.4f, 0.8f, 0.5f },
     };
 
     GLuint indices[NumIndices] = {
+
         // Frente
-        0, 1, 2, 1, 2, 3,
+        0, 1, 2, 1, 3, 2,
         // Direita
         1, 3, 7, 1, 5, 7,
         // Baixo
@@ -159,7 +174,7 @@ void init(void) {
     GLint coresId = glGetProgramResourceLocation(program, GL_PROGRAM_INPUT, "vColors");
 
     glBindBuffer(GL_ARRAY_BUFFER, Buffers[0]);
-    glVertexAttribPointer(coordsId, 2, GL_FLOAT, GL_FALSE, 0, nullptr);
+    glVertexAttribPointer(coordsId, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
 
     glBindBuffer(GL_ARRAY_BUFFER, Buffers[1]);
     glVertexAttribPointer(coresId, 3, GL_FLOAT, GL_FALSE, 0, nullptr);
@@ -169,16 +184,16 @@ void init(void) {
 }
 
 void display(void) {
-    static const float black[] = {
-        0.0f, 0.0f, 0.0f, 0.0f
-    };
+    //static const float black[] = {
+    //    0.0f, 0.0f, 0.0f, 0.0f
+    //};
 
-    glClearBufferfv(GL_COLOR, 0, black);
+    //glClearBufferfv(GL_COLOR, 0, black);
 
     // Vincula (torna ativo) o VAO
     glBindVertexArray(VAO);
 
-    glDrawElements(GL_TRIANGLES, NumIndices, GL_UNSIGNED_INT, (void*)0);
+    glDrawElements(GL_TRIANGLES, NumIndices, GL_UNSIGNED_INT, (void *)0);
 }
 
 void print_error(int error, const char* description) {
