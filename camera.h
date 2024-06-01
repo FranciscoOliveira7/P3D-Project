8415e6329e1891faa48afc7bb8295aea4c063ec6
Francisco Oliveira <64752398+Sincopse@users.noreply.github.com>
2024-06-01 21:14:13 +0100

    Update camera.h

--- camera.h
+++ camera.h
@@ -8,50 +8,50 @@ using namespace glm;
 class Camera {
 public:
 
-    // Construtor que inicializa a c‚mera com a raz„o de aspecto fornecida
+    // Construtor que inicializa a c√¢mera com o ratio fornecido
     Camera(float ratio) : ratio_{ ratio } {
-        center_ = vec3(0.0f, -5.0f, 0.0f);  // Ponto para onde a c‚mera est· olhando
-        up_ = vec3(0.0f, 1.0f, 0.0f);       // Vetor que define a direÁ„o "para cima"
+        center_ = vec3(0.0f, -5.0f, 0.0f);  // Ponto para onde a c√¢mera est√° olhando
+        up_ = vec3(0.0f, 1.0f, 0.0f);       // Vetor que define a dire√ß√£o "para cima"
 
-        // Configura a matriz de visualizaÁ„o
+        // Configura a matriz de visualiza√ß√£o
         view_ = lookAt(vec3(0.0f), center_, up_);
-        // Configura a matriz de projeÁ„o
+        // Configura a matriz de proje√ß√£o
         projection_ = perspective(radians(45.0f), ratio, 1.0f, 100.0f);
     }
 
-    // Define a posiÁ„o da c‚mera
+    // Define a posi√ß√£o da c√¢mera
     void SetPosition(float x, float y, float z) {
         view_ = lookAt(vec3(x, y, z), center_, up_);
     }
 
-    // Define o campo de vis„o da c‚mera
+    // Define o campo de vis√£o da c√¢mera
     void SetFov(float fov) {
         fov_ = fov;
         UpdateProjection();
     }
 
-    // Define a raz„o de aspecto da c‚mera
+    // Define a raz√£o de aspecto da c√¢mera
     void SetRatio(float ratio) {
         ratio_ = ratio;
         UpdateProjection();
     }
 
-    // Retorna a matriz de projeÁ„o
+    // Retorna a matriz de proje√ß√£o
     mat4 GetProjectionMatrix(void) const { return projection_; }
 
-    // Retorna a matriz de visualizaÁ„o
+    // Retorna a matriz de visualiza√ß√£o
     mat4 GetViewMatrix(void) const { return view_; }
 
 private:
-    // Atualiza a matriz de projeÁ„o com os novos par‚metros
+    // Atualiza a matriz de proje√ß√£o com os novos par√¢metros
     void UpdateProjection() { projection_ = perspective(radians(fov_), ratio_, 1.0f, 100.0f); }
 
-    vec3 center_;   // Ponto para onde a c‚mera est· olhando
-    vec3 up_;       // Vetor que define a direÁ„o "para cima"
+    vec3 center_;   // Ponto para onde a c√¢mera est√° olhando
+    vec3 up_;       // Vetor que define a dire√ß√£o "para cima"
 
-    float ratio_ = 0.0f;  // Raz„o de aspecto da c‚mera
-    float fov_ = 45.0f;   // Campo de vis„o da c‚mera
+    float ratio_ = 0.0f;  // Raz√£o de aspecto da c√¢mera
+    float fov_ = 45.0f;   // Campo de vis√£o da c√¢mera
 
-    mat4 projection_;  // Matriz de projeÁ„o
-    mat4 view_;        // Matriz de visualizaÁ„o
+    mat4 projection_;  // Matriz de proje√ß√£o
+    mat4 view_;        // Matriz de visualiza√ß√£o
 };
